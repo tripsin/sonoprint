94e33e4eb62849cd04f4ce9d45a865b6b51101ec
Roman Orekhov <tripsin@yandex.ru>
2021-03-02 22:43:16 +0500

    refactoring: make report with no classes.2

--- report.py
+++ report.py
@@ -21,129 +21,114 @@ def mm_to_pix(mm: int):
     return int(mm * PRINTER_DPI / 25.4)
 
 
-class Report:
-    def __init__(self, printer: QPrinter, viewer: DicomImageList):
-        self.printer = printer
-        self.printer.setResolution(PRINTER_DPI)
-        self.printer.setColorMode(QPrinter.ColorMode.GrayScale)
-        self.printer.setWinPageSize(QPrinter.A4)
+def make(printer: QPrinter, viewer: DicomImageList):
+    printer.setResolution(PRINTER_DPI)
+    printer.setColorMode(QPrinter.ColorMode.GrayScale)
+    printer.setWinPageSize(QPrinter.A4)
 
-        self.viewer = viewer
-        self.images = viewer.widget_iterator()
+    images = viewer.widget_iterator()
 
-        self.paper_width = mm_to_pix(printer.widthMM())
-        self.paper_height = mm_to_pix(printer.heightMM())
+    painter = QPainter()
 
-        self.center_rect = QRect(mm_to_pix(LEFT_MARGIN),
-                                 mm_to_pix(TOP_HEIGHT),
-                                 self.paper_width - mm_to_pix(LEFT_MARGIN + RIGHT_MARGIN),
-                                 self.paper_height - mm_to_pix(TOP_HEIGHT + BOTTOM_HEIGHT))
+    # --------------------------- grid calculating ----------------------------
+    paper_width = mm_to_pix(printer.widthMM())
+    paper_height = mm_to_pix(printer.heightMM())
 
-        self.columns = self.center_rect.width() // mm_to_pix(MIN_ITEM_WIDTH)
-        self.rows = self.center_rect.height() // mm_to_pix(MIN_ITEM_HEIGHT)
+    center_rect = QRect(mm_to_pix(LEFT_MARGIN),
+                        mm_to_pix(TOP_HEIGHT),
+                        paper_width - mm_to_pix(LEFT_MARGIN + RIGHT_MARGIN),
+                        paper_height - mm_to_pix(TOP_HEIGHT + BOTTOM_HEIGHT))
 
-        if (self.columns == 0) or (self.rows == 0):
-            self.columns = 1
-            self.rows = 1
-            self.item_width = self.center_rect.width()
-            self.item_height = self.center_rect.height()
-        else:
-            self.item_width = int(self.center_rect.width() / self.columns)
-            self.item_height = int(self.center_rect.height() / self.rows)
+    columns = center_rect.width() // mm_to_pix(MIN_ITEM_WIDTH)
+    rows = center_rect.height() // mm_to_pix(MIN_ITEM_HEIGHT)
 
-    def _item_rect(self, top_left: QPoint):
-        return QRect(top_left.x() + mm_to_pix(BOX_MARGINS),
-                     top_left.y() + mm_to_pix(BOX_MARGINS),
-                     self.item_width - mm_to_pix(BOX_MARGINS * 2),
-                     self.item_height - mm_to_pix(BOX_MARGINS * 2))
+    if (columns == 0) or (rows == 0):
+        columns = 1
+        rows = 1
+        item_width = center_rect.width()
+        item_height = center_rect.height()
+    else:
+        item_width = int(center_rect.width() / columns)
+        item_height = int(center_rect.height() / rows)
+    # ------------------------- end (grid calculating) ------------------------
 
-    def make(self):
+    def _draw_top():
+        painter.drawLine(center_rect.left(),
+                         center_rect.top() - mm_to_pix(1),
+                         center_rect.right(),
+                         center_rect.top() - mm_to_pix(1))
 
-        painter = QPainter()
-        painter.begin(self.printer)
+    def _draw_bottom():
+        painter.drawLine(center_rect.left(),
+                         center_rect.bottom() + mm_to_pix(1),
+                         center_rect.right(),
+                         center_rect.bottom() + mm_to_pix(1))
 
-        def _draw_top():
-            painter.drawLine(self.center_rect.left(),
-                             self.center_rect.top() - mm_to_pix(1),
-                             self.center_rect.right(),
-                             self.center_rect.top() - mm_to_pix(1))
+        target_rect = QRect(QPoint(center_rect.left(),
+                                   center_rect.bottom() + mm_to_pix(2)),
+                            QSize(center_rect.width(),
+                                  mm_to_pix(BOTTOM_HEIGHT)))
 
-        def _draw_bottom():
-            painter.drawLine(self.center_rect.left(),
-                             self.center_rect.bottom() + mm_to_pix(1),
-                             self.center_rect.right(),
-                             self.center_rect.bottom() + mm_to_pix(1))
+        painter.setFont(QFont('Courier', 10))
+        painter.drawText(target_rect, Qt.AlignRight | Qt.AlignTop, viewer.device_info)
 
-            target_rect = QRect(QPoint(self.center_rect.left(),
-                                       self.center_rect.bottom() + mm_to_pix(2)),
-                                QSize(self.center_rect.width(),
-                                      mm_to_pix(BOTTOM_HEIGHT)))
+    def _draw_image_box(im: ImageBox, p: QPoint):
+        box_rect = QRect(p.x() + mm_to_pix(BOX_MARGINS),
+                         p.y() + mm_to_pix(BOX_MARGINS),
+                         item_width - mm_to_pix(BOX_MARGINS * 2),
+                         item_height - mm_to_pix(BOX_MARGINS * 2))
+        # -------------------------- header printing --------------------------
+        header_rect = QRect(box_rect.left(),
+                            box_rect.top(),
+                            box_rect.width(), 1)
 
-            f = QFont('Courier', 10)
-            painter.setFont(f)
-            br = painter.boundingRect(target_rect, Qt.AlignRight | Qt.AlignTop, self.viewer.device_info)
-            if target_rect.intersected(br) != br:
-                print('Out bounds!')
-            painter.drawText(target_rect, Qt.AlignRight | Qt.AlignTop, self.viewer.device_info)
+        painter.setFont(QFont('Courier', 10))
+        br = painter.boundingRect(header_rect,
+                                  Qt.AlignLeft | Qt.AlignBottom,
+                                  im.image_info)
+        header_rect.setHeight(br.height())
+        painter.drawText(header_rect,
+                         Qt.AlignLeft | Qt.AlignBottom,
+                         im.image_info)
+        # -------------------------- image printing ---------------------------
+        scaled_image = im.pixmap.scaled(box_rect.width(),
+                                        box_rect.height(),
+                                        Qt.KeepAspectRatio,
+                                        Qt.SmoothTransformation)
+        image_rect = QRect(box_rect.left(),
+                           box_rect.top() + header_rect.height() + 1,
+                           box_rect.width(),
+                           scaled_image.height())
+        painter.drawPixmap(image_rect, scaled_image)
+        # -------------------------- comment printing -------------------------
+        comment_rect = QRect(box_rect.left(),
+                             image_rect.top() + image_rect.height() + 1,
+                             box_rect.width(),
+                             box_rect.bottom() - image_rect.bottom())
+        painter.setFont(QFont('Arial', 12))
+        br = painter.boundingRect(comment_rect,
+                                  Qt.AlignLeft | Qt.AlignTop | Qt.TextWordWrap,
+                                  im.txt.text())
+        comment_rect.setHeight(br.height())
+        painter.drawText(comment_rect,
+                         Qt.AlignLeft | Qt.AlignTop | Qt.TextWordWrap,
+                         im.txt.text())
 
-        def _get_item_position():
-            while True:
-                _draw_top()
-                _draw_bottom()
-                for row in range(self.rows):
-                    for column in range(self.columns):
-                        x = column * self.item_width + self.center_rect.x()
-                        y = row * self.item_height + self.center_rect.y()
-                        yield QPoint(x, y)
-                self.printer.newPage()
+    def _page_engine():
+        while True:
+            _draw_top()
+            _draw_bottom()
+            for row in range(rows):
+                for column in range(columns):
+                    x = column * item_width + center_rect.x()
+                    y = row * item_height + center_rect.y()
+                    yield QPoint(x, y)
+            printer.newPage()
 
-        def _draw_image_box(im: ImageBox, p: QPoint):
-
-            box_rect = QRect(p.x() + mm_to_pix(BOX_MARGINS),
-                             p.y() + mm_to_pix(BOX_MARGINS),
-                             self.item_width - mm_to_pix(BOX_MARGINS * 2),
-                             self.item_height - mm_to_pix(BOX_MARGINS * 2))
-
-            header_rect = QRect(box_rect.left(),
-                                box_rect.top(),
-                                box_rect.width(), 1)
-
-            painter.setFont(QFont('Courier', 10))
-            br = painter.boundingRect(header_rect,
-                                      Qt.AlignLeft | Qt.AlignBottom,
-                                      im.image_info)
-            header_rect.setHeight(br.height())
-            painter.drawText(header_rect,
-                             Qt.AlignLeft | Qt.AlignBottom,
-                             im.image_info)
-
-            scaled_image = im.pixmap.scaled(box_rect.width(),
-                                            box_rect.height(),
-                                            Qt.KeepAspectRatio,
-                                            Qt.SmoothTransformation)
-            image_rect = QRect(box_rect.left(),
-                               box_rect.top() + header_rect.height() + 1,
-                               box_rect.width(),
-                               scaled_image.height())
-            painter.drawPixmap(image_rect, scaled_image)
-
-            comment_rect = QRect(box_rect.left(),
-                                 image_rect.top() + image_rect.height() + 1,
-                                 box_rect.width(),
-                                 box_rect.bottom() - image_rect.bottom())
-            painter.setFont(QFont('Arial', 12))
-            br = painter.boundingRect(comment_rect,
-                                      Qt.AlignLeft | Qt.AlignTop | Qt.TextWordWrap,
-                                      im.txt.text())
-            comment_rect.setHeight(br.height())
-            painter.drawText(comment_rect,
-                             Qt.AlignLeft | Qt.AlignTop | Qt.TextWordWrap,
-                             im.txt.text())
-
-        point = _get_item_position()
-        image: ImageBox
-        for image in self.images:
-            if image.isChecked():
-                _draw_image_box(image, next(point))
-
-        painter.end()
+    painter.begin(printer)
+    point = _page_engine()
+    image: ImageBox
+    for image in images:
+        if image.isChecked():
+            _draw_image_box(image, next(point))
+    painter.end()
